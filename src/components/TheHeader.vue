0bd1ff9d67472f510258d00b67a536e3856de9a0
Mikhail Shishov <shishovmike96@gmail.com>
2026-01-18 17:53:29 +0100

    feat: first component, i18n

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+export default {
+  name: "TheHeader",
+  setup() {
+
+  }
+}
+</script>
+
+<template>
+  <header class="header">
+    <div class="container">
+      <nav class="navbar">
+        <RouterLink to="/" class="logo">
+          <img src="/logo.svg" width="130" height="24" alt="UKF" />
+        </RouterLink>
+        <ul class="navbar__list">
+          <li><a href="#about">About</a></li>
+          <li><a href="#benefits">Benefits</a></li>
+          <li><a href="form">Form</a></li>
+        </ul>
+      </nav>
+
+    </div>
+  </header>
+</template>
+
+<style lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
+
+const i18n = createI18n({
+  // something vue-i18n options here ...
+})
 
 import App from './App.vue'
 import router from './router'
@@ -9,4 +14,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+app.use(i18n)
 app.mount('#app')
